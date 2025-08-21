155ffa8953728a4bb8ef00c02c33ad41b7fc3957
Matt Wiese <github@mattwie.se>
2025-08-21 14:12:01 -0400

    Remove gene family placeholder

--- assets/js/graphql/get-genes.js
+++ assets/js/graphql/get-genes.js
@@ -127,11 +127,9 @@ export function genesDataToSearchResults(data) {
   // flatten results
   const results =
     data.genes.results.map(({organism: {genus, species}, strain, ...gene}) => {
-//      const geneFamilyAssignments =
-//        gene.geneFamilyAssignments
-//          .map(({geneFamily: {identifier}}) => identifier);
-      const geneFamilyAssignments = gene.geneFamilyAssignments
-        .map(({ geneFamily }) => geneFamily?.identifier ?? 'CTCPLACEHOLDER');
+      const geneFamilyAssignments =
+        gene.geneFamilyAssignments
+          .map(({geneFamily: {identifier}}) => identifier);
       const panGeneSets =
         gene.panGeneSets
           .map(({identifier}) => identifier);
